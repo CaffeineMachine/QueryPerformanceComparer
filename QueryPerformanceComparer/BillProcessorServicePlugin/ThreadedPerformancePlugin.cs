ccb7a71253d534bd829de90c0d7da495a4c3ea23
CaffeineMachine <aznslacker2004@gmail.com>
2015-02-02 20:41:02 -0600

    Add ThreadedPerformanceSessionPlugin Project.
    Adds the asynchronous threaded performance session plugin.

---
+++ QueryPerformanceComparer/BillProcessorServicePlugin/ThreadedPerformancePlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using QueryPerformance.Interfaces;
+
+namespace QueryPerformance.ThreadedPerformanceSessionPlugin
+{
+    public class ThreadedPerformancePlugin : IPlugin
+    {
+        public ThreadedPerformancePlugin() : this(new ThreadedPerformanceSessionControl())
+        {
+            
+        }
+
+        public ThreadedPerformancePlugin(UserControl control)
+        {
+            Control = control;
+        }
+
+        public UserControl Control { get; private set; }
+
+        public string DisplayName
+        {
+            get { return "Threaded Url Performance Tests"; }
+        }
+    }
+}
